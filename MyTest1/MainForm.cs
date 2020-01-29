55a8ebf5ccf8a201198bf3539cb5b55e9cc017e6
vitaliy.moseev <vetal.moseev>
2020-01-29 10:02:06 +0500

    first commit

---
+++ MyTest1/MainForm.cs
@@ -0,0 +1,38 @@
+﻿/*
+ * Created by Ranorex
+ * User: vitaliy.moseev
+ * Date: 27.01.2020
+ * Time: 17:34
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+	
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+
+namespace MyTest1
+{
+	/// <summary>
+	/// Description of MainForm.
+	/// </summary>
+	public partial class MainForm : System.Windows.Forms.Form
+	{
+		public MainForm()
+		{
+			//
+			// The InitializeComponent() call is required for Windows Forms designer support.
+			//
+			InitializeComponent();
+			
+			// TODO: Add constructor code after the InitializeComponent() call.
+
+			// TODO: Add Code to call testing functions - for example:	
+			// Automation.Start()
+
+		}
+	}
+}
